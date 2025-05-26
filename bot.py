d83f78de42f03d86fd2f68bcd5c91fe51d628a10
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2025-05-26 23:18:23 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ from utils import progress
 
 API_ID = int(os.getenv("API_ID", "10811400"))     # Replace with your API_ID
 API_HASH = os.getenv("API_HASH", "191bf5ae7a6c39771e7b13cf4ffd1279")  # Replace with your API_HASH
-BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")  # Replace with your Bot Token
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7097361755:AAE_zegGSo1OsWQWsy7G8eit4pDXFxOj7I8")  # Replace with your Bot Token
 
 app = Client("enhance_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
