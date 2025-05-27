8112cfca6d09bf40e3580ce1a48f12f6e44aee65
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2025-05-27 17:19:35 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ from utils import progress, humanbytes, time_formatter
 API_ID = int(os.getenv("API_ID", "10811400"))
 API_HASH = os.getenv("API_HASH", "191bf5ae7a6c39771e7b13cf4ffd1279")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "7097361755:AAHJcqT4_YBvSq5hG7FwP5kDhugFBTwfRQE")
-LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1002067650699")
+LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1002067650699"))
 
 app = Client("enhance_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 MAX_FILE_SIZE = 300 * 1024 * 1024
