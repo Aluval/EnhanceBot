b100bb7b90e1b943878e207eb8e64e05e33cee49
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2025-05-27 13:06:09 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,12 +3,15 @@ import math
 from pyrogram.types import Message
 
 # Progress function
-async def progress(current, total, message, start):
+async def progress(current, total, message, start, *args):
     percent = int(current * 100 / total)
     elapsed = time() - start
     speed = current / elapsed if elapsed > 0 else 0
     eta = (total - current) / speed if speed > 0 else 0
-    await message.edit_text(
-        f"⬇️ Downloading... {percent}%\n"
-        f"Speed: {speed / 1024:.2f} KB/s\nETA: {int(eta)}s"
-    )
+    try:
+        await message.edit_text(
+            f"⬇️ Progress: {percent}%\n"
+            f"Speed: {speed / 1024:.2f} KB/s\nETA: {int(eta)}s"
+        )
+    except:
+        pass
