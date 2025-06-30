dba437148e21b0be17d8cbdc162e9e0b9b855d6e
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2025-06-30 22:41:49 +0530

    ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24

---
+++ config.py
@@ -0,0 +1,18 @@
+#ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24
+import re
+from os import environ
+import os
+
+id_pattern = re.compile(r'^.\d+$')
+
+
+API_ID = os.environ.get("API_ID", "10811400")
+API_HASH = os.environ.get("API_HASH", "191bf5ae7a6c39771e7b13cf4ffd1279")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7097361755:AAHJcqT4_YBvSq5hG7FwP5kDhugFBTwfRQE")
+ADMIN = int(os.environ.get("ADMIN", '6469754522'))
+#ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24
+SUNRISES_PIC= "https://graph.org/file/bd91761f6e938e2e6d23a.jpg"  # Replace with your Telegraph link
+UPDATES_CHANNEL = os.getenv("UPDATES_CHANNEL", "https://t.me/Sunrises24BotUpdates") # Replace with your Updates link
+SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "https://t.me/Sunrises24BotSupport") # Replace with your Support link
+WEBHOOK = bool(os.environ.get("WEBHOOK", True))
+PORT = int(os.environ.get("PORT", "8080"))
