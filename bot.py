2d13a88a2853289d57a6a2f44da6b1a58ef826a3
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2025-06-30 23:07:24 +0530

    ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ class Bot(Client):
 
     def __init__(self):
         super().__init__(
-            name="Enhance24Bot",
+            name="PixelPulseBot",
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
