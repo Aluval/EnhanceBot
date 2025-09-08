4b1b04a1e122c8a0a2d671e2084fbab48135b72d
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2025-09-08 12:38:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,8 +11,8 @@ API_HASH = os.environ.get("API_HASH", "191bf5ae7a6c39771e7b13cf4ffd1279")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "7871689536:AAEhj97j5TszB10j7KHkEa_UadXEiJkD8ac")
 ADMIN = int(os.environ.get("ADMIN", '6469754522'))
 #ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24
-SUNRISES_PIC= "https://envs.sh/u0Z.jpg/IMG20250701161.jpg" # Replace with your Telegraph link - Start Pic
-INFO_PIC= "https://envs.sh/u05.jpg/IMG20250701173.jpg" # Replace with your Telegraph link - Information 
+SUNRISES_PIC= "https://envs.sh/big.jpg" # Replace with your Telegraph link - Start Pic
+INFO_PIC= "https://envs.sh/biM.jpg" # Replace with your Telegraph link - Information 
 UPDATES_CHANNEL = os.getenv("UPDATES_CHANNEL", "https://t.me/Sunrises24BotUpdates") # Replace with your Updates link
 SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "https://t.me/Sunrises24BotSupport") # Replace with your Support link
 WEBHOOK = bool(os.environ.get("WEBHOOK", True))
