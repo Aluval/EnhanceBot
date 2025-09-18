3636f811aef76792bc7584687f2c794e5b4c94d1
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2025-09-18 23:48:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ id_pattern = re.compile(r'^.\d+$')
 
 API_ID = os.environ.get("API_ID", "10811400")
 API_HASH = os.environ.get("API_HASH", "191bf5ae7a6c39771e7b13cf4ffd1279")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7871689536:AAEhj97j5TszB10j7KHkEa_UadXEiJkD8ac")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7871689536:AAGjSkOtVe4iMWR8IegoylE9qfaDIXSTcgo")
 ADMIN = int(os.environ.get("ADMIN", '6469754522'))
 #ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24
 SUNRISES_PIC= "https://envs.sh/big.jpg" # Replace with your Telegraph link - Start Pic
